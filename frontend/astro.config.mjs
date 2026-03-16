481e485a4f55a28812c6c727f1ceb39e9627cdea
dinesh-g1 <dineshgillalaa@gmail.com>
2026-03-16 11:40:10 +0530

    chore: fixing frontend modules

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'hybrid',
+  adapter: node({ mode: 'standalone' }),
   server: { port: 3000, host: true },
-});
+});;
